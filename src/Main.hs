8dd0391298c7ca0a5079b2d449547951d2e00757
prey169 <francisdrossi@gmail.com>
2025-06-19 17:01:52 -0400

    quicksort recursion

--- src/Main.hs
+++ src/Main.hs
@@ -89,3 +89,10 @@ cylinder r h =
 
 calcDensities :: (RealFloat a) => [(a, a)] -> [a]
 calcDensities xs = [density | (m, v) <- xs, let density = m / v]
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+  in smallerSorted ++ [x] ++ biggerSorted
