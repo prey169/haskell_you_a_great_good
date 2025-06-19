110fa3af6883141648dad3ad939030e906f83d3b
prey169 <francisdrossi@gmail.com>
2025-06-18 23:22:38 -0400

    chapter 3 done

--- src/Main.hs
+++ src/Main.hs
@@ -21,3 +21,21 @@ removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
 triangles = [(a, b, c)| c <- [1..10], b <- [1..10], a <- [1..10]]
 rightTriangles = [(a, b, c)| c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
 rightTriangles' = [(a, b, c)| c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b+ c == 24]
+
+-- Proper way to create functions
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+lowestInt = minBound :: Int
+largestChar = maxBound :: Char
+
+largestInTuple = maxBound :: (Bool, Int, Char)
