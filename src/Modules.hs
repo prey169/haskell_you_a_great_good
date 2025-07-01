ae62f09d180feff079c679bf6aff47f7983e40f4
prey169 <francisdrossi@gmail.com>
2025-07-01 16:52:34 -0400

    added a person 'record syntax' aka kinda like a struct

--- src/Modules.hs
+++ src/Modules.hs
@@ -29,4 +29,11 @@ surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
 surface (Rectangle (Point x1 y1) (Point x2 y2)) = abs (x2 - x1) * abs (y2 - y1)
 
-
+-- Record syntax
+data Person = Person { firstName :: String
+        , lastName :: String
+        , age :: Int
+        , height :: Float
+        , phoneNumber :: String
+        , flavor :: String
+        } deriving (Show)
