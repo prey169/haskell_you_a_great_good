f4d9ba132b49d0207700eb0180694593b1483892
prey169 <francisdrossi@gmail.com>
2025-08-02 11:28:08 -0400

    chapter 9: idk why the book said to do that, when this is better lul

--- src/guess_number.hs
+++ src/guess_number.hs
@@ -10,7 +10,7 @@ askForNumber gen = do
   let (randNumber, newGen) = randomR (1,10) gen :: (Int, StdGen)
   putStr "Which number in the range from 1 to 10 am I thinking of? "
   numberString <- getLine
-  when (not $ null numberString) $ do
+  when (null numberString) $ do
     let number = read numberString
     if randNumber == number
       then putStrLn "You are correct!!!"
